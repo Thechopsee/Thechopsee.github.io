d2e47abaa0e2536dc28ea420bd0815560bcea1e7
Thechopsee <sebastian.walent@gmail.com>
2022-01-28 00:14:53 +0100

    dsd

--- modal_galery.js
+++ modal_galery.js
@@ -2,14 +2,12 @@ function prepareModal()
 {
 var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var pole_obrazku = document.getElementsByClassName("myImg");
 for(var i=0;i<pole_obrazku.length;i++)
 {
     pole_obrazku[i].onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
-        //captionText.innerHTML = this.alt;
         }
 }
 var span = document.getElementsByClassName("close")[0];
@@ -17,4 +15,3 @@ span.onclick = function() {
     modal.style.display = "none";
   }
 }
-// When the user clicks on <span> (x), close the modal
