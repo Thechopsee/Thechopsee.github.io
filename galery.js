cd5d9e34abc70b8643d21237e32f6f5ca2a4e11f
Thechopsee <sebastian.walent@gmail.com>
2022-06-04 00:05:09 +0200

    small history update

--- galery.js
+++ galery.js
@@ -15,3 +15,12 @@ span.onclick = function() {
     modal.style.display = "none";
   }
 }
+
+function down_vysledky(a)
+{
+  link = document.createElement("a"); //create 'a' element
+  link.setAttribute("href", "vysledky/"+a.id+".xlsx"); //replace "file" with link to file you want to download
+  link.setAttribute("download", "vysledky/"+a.id+".xlsx");// replace "file" here too
+  link.click(); //virtually click <a> element to initiate download
+  
+}
